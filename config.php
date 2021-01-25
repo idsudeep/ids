37bb719ae0f38ef1ab3ba6f5ddfa485526a64a05
idsudeep <sudeep.numb@gmail.com>
2021-01-25 16:10:52 +0530

    0004

--- config.php
+++ config.php
@@ -1,67 +1,6 @@
 
 <?php
 
-function multiple_insert($table, $fields = array(), $values = array(), $appendix = false, $ret = false) {
-
-$query = 'INSERT INTO';
-$query .= ' `' . $this->escape($table) . "`";
-
-if (is_array($fields)) {
-    $query .= ' (';
-    $num = 0;
-    foreach ($fields as $key => $value) {
-        $query .= ' `' . $value . '`';
-        $num++;
-        if ($num != count($fields)) {
-            $query .= ',';
-        }
-    }
-    $query .= ' ) VALUES ';
-
-    foreach ($values as $key => $value) {
-        $query .= '(';
-        foreach ($value as $key => $value) {
-            $query .= "'" . $value . "'" . ',';
-        }
-        $query = rtrim($query, ',');
-        $query .= '),';
-    }
-
-    $query = rtrim($query, ',');
-
-    if ($appendix) {
-        $query .= ' ' . $appendix;
-    }
-    if ($ret) {
-        return $query;
-    }
-
-    $this->sql    = $query;
-    $this->result = mysqli_query($this->mysqli, $query);
-    if (mysqli_error($this->mysqli) != '') {
-        $this->_error(mysqli_error($this->mysqli));
-        $this->result = null;
-        return false;
-    } else {
-        return $this;
-    }
-
-}
-}
-
-function catch_query_error ($query , $self ="something mixed ")
-{
-	if($result = mysqli_query($query)){ return $result;
-	
-	
-	}
-
-	
-	else{ throw new Expection($self);}
-}
-
-
-
 /* mysql://bf05ca120624fb:181ac24a@us-cdbr-east-02.cleardb.com/heroku_4e956cc525a6de9?reconnect=true */
 $servername ="localhost";
 $username ="root";
